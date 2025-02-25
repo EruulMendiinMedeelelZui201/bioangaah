0f90e76545995bb6aa6cd3bf00895a54524b73f8
Эрүүл мэндийн мэдээлэл зүй 201 <tsolmonkhanorgil@gmail.com>
2025-02-25 19:13:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// Handle form redirection for sign-up / sign-in links
+document.getElementById('redirectSignUp').addEventListener('click', function () {
+    window.location.href = 'login.html';  // Redirect to sign-up page
+});
+
+document.getElementById('redirectSignIn').addEventListener('click', function () {
+    window.location.href = 'login.html';  // Redirect to login page
+});
