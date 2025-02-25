feeae686d55b292676710ca8ea035ff7dbca5c52
Эрүүл мэндийн мэдээлэл зүй 201 <tsolmonkhanorgil@gmail.com>
2025-02-25 20:37:51 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Handle form redirection for sign-up / sign-in links
-document.getElementById('redirectSignUp').addEventListener('click', function () {
-    window.location.href = 'login.html';  // Redirect to sign-up page
-});
-
-document.getElementById('redirectSignIn').addEventListener('click', function () {
-    window.location.href = 'login.html';  // Redirect to login page
-});
